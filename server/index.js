9a6f0570b8104e83ba3efbd89ea7b93156e9679d
Afridi <afridi.hq97@gmail.com>
2024-04-06 13:37:07 +0530

    backend first commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
-const express = require('express');
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+
+
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+try {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("Connected to mongoDb");
+} catch (error) {
+  throw error;
+}
+}
 
 app.listen(8000, () => {
+    connect()
     console.log("Connected to backend:");
 });
